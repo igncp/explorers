177aa683fc81f1439a46207fab1c59046ce8eccb
Ignacio Carbajo <icarbajop@gmail.com>
2016-05-29 19:24:31 +0100

    Update install script

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -1,6 +1,10 @@
-# Install many of the tools in a Ubuntu distribution
+#!/bin/sh
+
+# Install many of the tools in a Ubuntu distribution,
 
 sudo apt-get install -y git-extras jq
-sudo apt-get remove -y cloc
 
-npm i -g cloc # more recent version than with apt-get
+# This requires node, it is a more recent version
+# of the apt-get one.
+sudo apt-get remove -y cloc
+npm i -g cloc
